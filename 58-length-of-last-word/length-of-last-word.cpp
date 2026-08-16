6ad0f437169f0140b8484e222696a0a8c601dbae
Varuzhan Manukyan <129852982+VaruzhanManukyan@users.noreply.github.com>
2026-08-17 02:44:06 +0400

    Time: 0 ms (100.00%) | Memory: 8.8 MB (92.95%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int i = static_cast<int>(s.size()) - 1;
+        short i = static_cast<short>(s.size()) - 1;
 
         while (i >= 0 && s[i] == ' ') {
             --i;
